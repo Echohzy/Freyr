9609483166ee37a74e39b01eb2968312e455fafb
ziyi <ziyi.huang@ceasia.com.cn>
2017-12-15 18:18:07 +0800

    style

--- src/javascripts/components/home.js
+++ src/javascripts/components/home.js
@@ -2,16 +2,28 @@
 
 import React, { Component } from 'react';
 
+import Tabs from './tabs.js';
+
+const { Item } = Tabs;
+
 import '../../stylesheets/home.less';
 
 export default class Home extends Component {
   constructor(props){
     super(props);
+    this.state = {
+      activeKey: "books"
+    };
   }
   render(){
     return (
       <div className="home-container">
-        This is home page
+        <Tabs activeKey={this.state.activeKey}>
+          <Item itemKey="books" title={<button>Book</button>}>
+          </Item>
+          <Item itemKey="movies" title={<button>Movie</button>}>
+          </Item>
+        </Tabs>
       </div>
     );
   }
--- src/javascripts/components/tabs.js
+++ src/javascripts/components/tabs.js
@@ -5,14 +5,62 @@ import React, { Component } from 'react';
 export default class Tabs extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      current_tab: this.props.default_tab
-    }
+  }
+  renderNav() {
+    return React.Children.map(this.props.children, (child) => {
+      let { itemKey, title} = child.props;
+      let itemNavClassName  = ["tab-button"];
+      if (itemKey === this.props.activeKey) {
+        itemNavClassName.push('active');
+      }
+      return (
+        <div className={itemNavClassName.join(" ")}>
+          {title}
+        </div>
+      );
+    });
+  }
+  renderItems() {
+    return React.Children.map((child) => {
+      let { itemKey, children } = child.props;
+      return (
+        <Item active={this.props.activeKey===itemKey}>
+          {children}
+        </Item>
+      );
+
+    });
   }
   render() {
     return (
-      <div>
+      <div className="tabs-container">
+        <div className="tabs-buttons">
+          {this.renderNav()}
+        </div>
+        <div className="tabs-content">
+          {this.renderItems()}
+        </div>
       </div>
     );
   }
 }
+
+class Item extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    let classname = ["tab-item-container"];
+    this.props.active ? classname.push('active') : "";
+    return (
+      <div className={classname.join(" ")}>
+        {this.props.children}
+      </div>
+    );
+  }
+}
+
+
+Tabs.Item = Item;
+
+

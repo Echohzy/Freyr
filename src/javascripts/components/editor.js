c4e15bf3dac3dd07cea73d4d7735cd676a03cb03
ziyi <ziyi.huang@ceasia.com.cn>
2018-01-24 17:31:57 +0800

    add editor

---
+++ src/javascripts/components/editor.js
@@ -0,0 +1,12 @@
+'use strict';
+
+import React, { Component } from 'react';
+
+export default class Editor extends Component {
+  render() {
+    return (
+      <div className="editor-container">
+      </div>
+    );
+  }
+}

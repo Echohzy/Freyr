25e1920bcdee5b35246a22ae802a5ea285f9fbcc
Echohzy <1820123540@qq.com>
2018-06-16 22:32:35 +0800

    handle mobx

--- routes/page.js
+++ routes/page.js
@@ -1,45 +1,49 @@
 var express = require('express');
 var router = express.Router();
 
-router.get("/notifications", function(req, res, next){
-  res.render('notification', {title: 'Notification' });
-});
-
-router.get("/sign_up", function(req, res, next){
-  res.render('sign_up', {title: 'Sign Up' });
-});
-
-router.get("/sign_in", function(req, res, next){
-  res.render('sign_in', {title: 'Sign In'});
-});
-
-router.get("/reviews/:id", function(req, res, next) {
-  res.render('review', {title: 'Review', id: req.params.id });
-});
-
-router.get("/users/:id", function(req, res, next) {
-  res.render('user', {title: 'User', id: req.params.id });
-});
-
-router.get("/movies/:id", function(req, res, next) {
-  res.render('movie', { title: 'Movie', id: req.params.id });
-});
-
-router.get("/books/:id", function(req, res, next) {
-  res.render('book', { title: "Book", id: req.params.id });
-});
-
-router.get("/search", function(req, res, next) {
-  res.render('search', { title: "Freyr" });
-});
-
-router.get("/todo", function(req, res, next){
-  res.render('todo', { title: "Todo" });
-});
-
-/* GET home page. */
 router.get(/[^\n]*/, function(req, res, next) {
   res.render('index', { title: 'Freyr' });
 });
+
+// router.get("/notifications", function(req, res, next){
+//   res.render('notification', {title: 'Notification' });
+// });
+// 
+// router.get("/sign_up", function(req, res, next){
+//   res.render('sign_up', {title: 'Sign Up' });
+// });
+// 
+// router.get("/sign_in", function(req, res, next){
+//   res.render('sign_in', {title: 'Sign In'});
+// });
+// 
+// router.get("/reviews/:id", function(req, res, next) {
+//   res.render('review', {title: 'Review', id: req.params.id });
+// });
+// 
+// router.get("/users/:id", function(req, res, next) {
+//   res.render('user', {title: 'User', id: req.params.id });
+// });
+// 
+// router.get("/movies/:id", function(req, res, next) {
+//   res.render('movie', { title: 'Movie', id: req.params.id });
+// });
+// 
+// router.get("/books/:id", function(req, res, next) {
+//   res.render('book', { title: "Book", id: req.params.id });
+// });
+// 
+// router.get("/search", function(req, res, next) {
+//   res.render('search', { title: "Freyr" });
+// });
+// 
+// router.get("/todo", function(req, res, next){
+//   res.render('todo', { title: "Todo" });
+// });
+// 
+// /* GET home page. */
+// router.get(/[^\n]*/, function(req, res, next) {
+//   res.render('index', { title: 'Freyr' });
+// });
 
 module.exports = router;
--- src/javascripts/app/todo.js
+++ src/javascripts/app/todo.js
@@ -1,22 +1,24 @@
 'use strict';
 
-import { observable, autorun, computed, observer } from 'mobx';
+import { observable, autorun, computed } from 'mobx';
+
+import DevTools from 'mobx-react-devtools';
+
+import { observer } from 'mobx-react';
 
 import React, { Component } from 'react';
 
 import ReactDOM from 'react-dom';
 
 class TodoStore {
-  @observable todos = [];
+  @observable todos = [{task: "task1", completed: false, assignee: null}, {task: "task2", completed: false, assignee: null}];
   @observable pendingRequests = 0;
-
   constructor() {
     autorun(() => console.log(this.report));
   }
   @computed get completedTodoCount () {
     return this.todos.filter(todo => todo.completed === true).length;
   }
-
   @computed get report() {
     if(this.todos.length === 0)
       return "<none>";
@@ -36,6 +38,9 @@ class TodoStore {
 
 @observer
 class TodoList extends Component {
+  onNewTodo= () => {
+    this.props.store.addTodo(prompt('Enter a new todo:', 'coffee piz'));
+  } 
   render(){
     const {store} = this.props;
     return (
@@ -49,48 +54,54 @@ class TodoList extends Component {
         { store.pendingRequests > 0 ? <marquee>Loading</marquee> : null}
         <button onClick={this.onNewTodo}>New Todo</button>
         <small> (double-click a todo to edit) </small>
-        
+        <DevTools />
       </div>
     );
   }
-  onNewTodo= () => {
-    this.props.store.addTodo(prompt('Enter a new todo:', 'coffee piz'));
-  } 
+  
 }
 
 @observer
 class TodoView extends Component {
-  render() {
-    const { todo } = this.props;
-    return (
-      <li onDoubleClick={this,onRename}>
-        <input type="checkbox"  onchecked={todo.completed}  onChange={this.onToggleCompleted} />
-        { todo.task }
-        { todo.assignee?<small>{ todo }</small>:null } 
-        
-      <li>
-    );
-  }
   onRename= () => {
     const todo = this.props.todo;
-    todo.tack = prompt('Task name', todo.task) || todo.task;
+    todo.task = prompt('Task name', todo.task) || todo.task;
   }
-
   onToggleCompleted = () => {
     const todo = this.props.todo;
     todo.completed = !todo.completed;
+  }
+  render() {
+    const { todo } = this.props;
+    return (
+      <li onDoubleClick={this.onRename}>
+        <input type="checkbox"  checked={todo.completed}  onChange={this.onToggleCompleted} />
+        { todo.task }
+        { todo.assignee?<small>{ todo.assignee.name }</small>:null } 
+      </li>
+    );
   }
 }
 
 const todoStore = new TodoStore();
 
-ReactDOM.render(
-  <TodoList store={ todoStore } />,
-  document.getElementById('app')
-);
+ReactDOM.render(<TodoList store={ todoStore } />, document.getElementById('app'));
 
 
+var peopleStore = observable([
+  { name: "Michel" },
+  { name: "Me" }
+]);
 
+todoStore.todos[0].assignee = peopleStore[0];
+todoStore.todos[1].assignee = peopleStore[1];
+peopleStore[0].name = "Michel Weststrate";
+
+todoStore.pendingRequests++;
+setTimeout(function(){
+  todoStore.addTodo('Random todo:'+Math.random());
+  todoStore.pendingRequests--;
+}, 2000);
 
 
 // const todoStore = new TodoStore();

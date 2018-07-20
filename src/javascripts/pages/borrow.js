3e92d6515bbf09d783552062703e5fcbce908d45
ziyi <ziyi.huang@ceasia.com.cn>
2018-07-20 17:56:11 +0800

    handle borrow

---
+++ src/javascripts/pages/borrow.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+import GeneralHeader from '../components/general_header.js';
+
+import GeneraFooter from '../components/general_footer.js';
+
+class Borrow extends Component {
+  constructor(props){
+    super(props);
+  }
+  render(){
+    return (
+      <div className="borrow-container">
+      </div>
+    );
+  }
+}
+
+export default Borrow;
